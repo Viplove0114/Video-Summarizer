d0471f69f6bf57d44dfb914171284c2e91d6f358
viplove <viplovethakran4@gmail.com>
2025-08-24 11:03:40 +0530

    Add initial implementation of video summarizer app with file upload and analysis features

---
+++ app.py
@@ -0,0 +1,116 @@
+import streamlit as st
+from phi.agent import Agent
+from phi.model.google import Gemini
+from phi.tools.duckduckgo import DuckDuckGo
+from google.generativeai import upload_file, get_file
+import google.generativeai as genai
+import time
+from pathlib import Path
+
+import tempfile
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+
+API_KEY=os.getenv("GOOGLE_API_KEY")
+
+if API_KEY:
+    genai.configure(api_key=API_KEY)
+
+# Page configuration
+st.set_page_config(
+    page_title="Multimodal AI Agent- Video Summarizer",
+    page_icon="",
+    layout="wide"
+)
+
+st.title("PhiData Multimodal AI Agent 🎥🎤🖬 ")
+st.header("Powered by Gemini 2.0 Flash Exp")
+
+
+@st.cache_resource
+def initialize_agent():
+    return Agent(
+        name="AI Video Summarizer",
+        model=Gemini(id='gemini-2.5-flash'),
+        tools=[DuckDuckGo()],
+        markdown=True,
+    )
+
+
+## Initialize the agent
+multimodal_agent=initialize_agent()
+
+#File uploader
+video_file= st.file_uploader("Upload a video file", type=['mp4', 'mov', 'avi'], help="Upload a video for AI analysis")
+
+if video_file:
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as temp_video:
+        temp_video.write(video_file.read())
+        video_path = temp_video.name
+
+    st.video(video_path, format="video/mp4", start_time=0)
+
+    user_query = st.text_area(
+        "What insights are you seeking from the video?",
+        placeholder="Ask anything about the video content. The AI agent will analyze and gathe additional imformation for you",
+        help="Provide specific questions or insights to analyze the video."
+    )
+
+
+    if st.button("🔍 Analyze Video", key = "analyze_video_button"):
+        if not user_query:
+            st.warning("Please enter a question or insight to analyze the video.")
+        else:
+            try:
+                with st.spinner("Processing video and gathering insights..."):
+                    #upload and process video file
+                    processed_video = upoload_file(video_path)
+                    while processed_video.state.name == "PROCESSING":
+                        time.slepp(2)
+                        processed_video = get_file(processed_video.name)
+
+                    #prompt generation for analysis
+                    analysis_prompt = (
+                        f"""
+                        Analyze the uploaded video for content and context.
+                        Respond to the following query using video insights and supplementary web research.
+                        {user_query}
+
+                        Provide a detailed, user-friendly, and actionable response.
+                        """
+
+                    )
+
+                    #AI agent processing
+                    response = multimodal_agent.run(analysis_prompt, videos=[processed_video])
+
+                #display the result
+                st.subheader("Analysis Result")
+                st.markdown(response.content)
+
+            except Exception as error:
+                st.error(f"An error occured during analysis: {error}")
+
+            finally:
+                # clean up temporary video file
+                Path(video_path).unlink(missing_ok=True)
+
+
+else:
+    st.info("upload a video file to begin analysis.")
+
+# Customize text area height
+st.markdown(
+    """
+    <style>
+    .stTextArea textarea {
+        height: 100px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
